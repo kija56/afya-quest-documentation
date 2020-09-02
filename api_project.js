8820fb46bc6200c77d0e6e9bffbc01144932240f
kija56 <kijamabula2@gmail.com>
2020-09-02 12:34:49 +0300

    Modified routes

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-08-31T13:37:10.573Z",
+    "time": "2020-09-02T09:33:22.789Z",
     "url": "http://apidocjs.com",
     "version": "0.24.0"
   }
