7b31c97d3c032b58cd2a706371912e7a250cddb2
kija56 <kijamabula2@gmail.com>
2020-10-07 14:13:19 +0300

    API url edit

--- api_project.js
+++ api_project.js
@@ -1,6 +1,6 @@
 define({
   "title": "Afya Quest API Documentation",
-  "url": "https://afyaquest-api.herokuapp.com/v1",
+  "url": "https://afyaquest-api.herokuapp.com/api/v1",
   "name": "afyaquest-api",
   "version": "1.0.0",
   "description": "This is an API to serve the Afya Quest CMS and Afya Quest mobile application",
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-07T11:06:52.747Z",
+    "time": "2020-10-07T11:12:30.368Z",
     "url": "http://apidocjs.com",
     "version": "0.24.0"
   }
